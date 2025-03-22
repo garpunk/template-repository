cd43df0b2608f42450fd633db6c81ae09304dca8
garpunk <garpunked@gmail.com>
2025-03-22 13:43:14 -0600

    working on eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs}'] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+]);
+
+/*export default [
+  pluginJs.configs.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      semi: "error"
+    }
+  }
+];*/
